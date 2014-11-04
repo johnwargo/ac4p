137b346fed4d82f12aadd56247787c6adb863409
johnwargo <john@johnwargo.com>
2014-11-04 12:43:32 -0500

    Early November updates

---
+++ chapter 12/readme.md
@@ -0,0 +1,8 @@
+Chapter 12
+==========
+
+This part of the repository contains example files from Chapter 12 of Apache Cordova 4 Programming. This chapter covers the PhoneGap Build service, so the files contained herein will be sample project files used in the chapter. 
+
+***
+
+You can find information on many different topics on my [personal blog](http://www.johnwargo.com). Learn about all of my publications at [John Wargo Books](http://www.johnwargobooks.com). 
--- chapter 15/ex15.1/www/js/index.js
+++ chapter 15/ex15.1/www/js/index.js
@@ -1,49 +1,65 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+/* jshint browser: true */
+/*global console*/
+/*global alert*/
+/*global $*/
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//Some variables used by the application
+var hi, watchID;
+var appName = "Compass - ";
 
-        console.log('Received Event: ' + id);
-    }
+//Fires whenever there is an error - helps with toubleshooting.
+window.onerror = function (msg, url, line) {
+  var resStr;
+  var idx = url.lastIndexOf('/');
+  if (idx > -1) {
+    url = url.substring(idx + 1);
+  }
+  resStr = 'ERROR in ' + url + ' on line ' + line + ': ' + msg;
+  console.error(resStr);
+  alert(resStr);
+  return false;
 };
+
+function onBodyLoad() {
+  document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady() {
+  console.log('onDeviceReady fired.');
+  hi = document.getElementById('headingInfo');
+  //Setup the watch
+  //Read the compass every second (1000 milliseconds)
+  var watchOptions = {
+    frequency: 1000,
+    // filter : 1
+  };
+  console.log(appName + 'Creating watch: ' + JSON.stringify(watchOptions));
+  watchID = navigator.compass.watchHeading(onSuccess, onError, watchOptions);
+}
+
+function onSuccess(heading) {
+  console.log(appName + 'Received Heading');
+  console.log(appName + JSON.stringify(heading));
+  var hv = Math.round(heading.magneticHeading);
+  console.log(appName + 'Rotating to ' + hv + ' degrees');
+  $("#compass").rotate(-hv);
+  hi.innerHTML = '<b>Heading:</b> ' + hv + ' Degrees';
+}
+
+function onError(err) {
+  console.error(appName + 'Heading Error');
+  console.error(appName + 'Error: ' + JSON.stringify(err));
+  //Remove the watch since we're having a problem
+  navigator.compass.clearWatch(watchID);
+  //Clear the heading value from the page
+  // $('#headingInfo').replaceWith('<b>Heading:</b> None');
+  hi.innerHTML = '<b>Heading: </b>None';
+  //Then tell the user what happened.
+  if (err.code == CompassError.COMPASS_NOT_SUPPORTED) {
+    alert('Compass not supported.');
+  } else if (err.code == CompassError.COMPASS_INTERNAL_ERR) {
+    alert('Compass Internal Error');
+  } else {
+    alert('Unknown heading error!');
+  }
+}
