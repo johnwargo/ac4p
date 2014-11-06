9f616c1971829c4b9e6b837f70643a8a972f98c5
johnwargo <john@johnwargo.com>
2014-11-06 07:03:30 -0500

    split Ex17.1 into separate files

--- chapter 17/ex17.1 - Topcoat/index.js
+++ chapter 17/ex17.1 - Topcoat/index.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-  <title>Hello World #5 - Topcoat Version</title>
-  <meta charset="utf-8" />
-  <meta name="format-detection" content="telephone=no" />
-  <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
-  <link rel="stylesheet" type="text/css" href="css/topcoat-mobile-light.min.css">
-  <script src="cordova.js"></script>
-  <script>
     function onBodyLoad() {
       console.log("Entering onBodyLoad");
       alert("Body Load");
@@ -39,16 +29,3 @@
       console.log("Leaving onDeviceReady");
     }
   </script>
-</head>
-
-<body onload="onBodyLoad()">
-  <div class="topcoat-navigation-bar">
-    <div class="topcoat-navigation-bar__item center full">
-      <h1 class="topcoat-navigation-bar__title">Hello World #5</h1>
-    </div>
-  </div>
-  <p>This is a Cordova application that makes calls to the Cordova Device API.</p>
-  <p id="deviceInfo">Waiting for Cordova Initialization to complete.</p>
-</body>
-
-</html>
